03dd15c934aff1653fa0f1a494b352480e48b73f
claas <claas@141.22.77.154>
2019-12-18 10:30:06 +0100

    LOL

---
+++ AVS_P6/src/WorkerThread.java
@@ -0,0 +1,40 @@
+import java.util.List;
+import javax.swing.JLabel;
+import javax.swing.SwingWorker;
+
+public class WorkerThread extends SwingWorker<Integer, Integer>{
+	private JLabel label;
+    public WorkerThread (JLabel label)
+    {
+        this.label = label;
+    }
+
+    @Override
+    public Integer doInBackground()
+    {
+        int counter = 0;
+        while (counter < 100 && !isCancelled())
+        {
+            // Zeitintensive Aufgabe simulieren.
+            try
+            {
+                Thread.sleep (100);
+            }
+            catch (InterruptedException ex)
+            {
+            }
+            // Zwischenergebnis bereitstellen.
+            publish (++counter);
+        }
+        return counter;
+    }
+    // Wird vom Event-Dispatch-Thread aufgerufen.
+    protected void process (List<Integer> zwischenergebnisse)
+    {
+        // Für jedes einzelne Zwischenergebnis aus der Liste.
+        for (Integer zwischenergebnis : zwischenergebnisse)
+        {
+            label.setText (zwischenergebnis.toString());
+        }
+    }
+}
